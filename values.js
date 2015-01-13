80c09816b9edaba387f419c7e2d1614a5a7c028b
Alex Dorey <dorey415@gmail.com>
2015-01-12 20:57:32 -0500

    trying out the suggested rearrangement of the values (re @nneonneo's PR #14)

--- values.js
+++ values.js
@@ -22,3 +22,29 @@ var values = [
     [1],
     "`parseFloat('nan')`"
 ];
+
+if ((""+window.location.search).match(/reordered/)) {
+    values = [
+        false,
+        0,
+        "`'0'`",
+        [0],
+        "",
+        "`[]`",
+        [[]],
+        true,
+        1,
+        "`'1'`",
+        [1],
+        -1,
+        "`'-1'`",
+        "`'true'`",
+        "`'false'`",
+        Infinity,
+        -Infinity,
+        "`null`",
+        "`undefined`",
+        "`{}`",
+        "`parseFloat('nan')`"
+    ];
+}
